df6368e7cc4aea31463f1cb361fb34351188de7a
finch18 <pawel.marek87@gmail.com>
2021-01-23 19:40:17 +0100

    Project initialization templete

---
+++ GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 23.01.2021 19:33:04
+ * Author : EBOHOC
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
